4c2ea7ebafa757f7f861ad37c53a8ba176639766
vpslinuxinstall <38807387+vpslinuxinstall@users.noreply.github.com>
2022-01-16 02:50:53 -0600

    Update process.php

--- process.php
+++ process.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_FILES['files'])) {
         $errors = [];
         $path = 'uploads/';
-	$extensions = ['jpg', 'jpeg', 'png', 'gif'. 'zip', 'pdf'];
+// 	$extensions = ['jpg', 'jpeg', 'png', 'gif'. 'zip', 'pdf'];
 		
         $all_files = count($_FILES['files']['tmp_name']);
 
@@ -18,13 +18,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 		$file = $path . $file_name;
 
-		if (!in_array($file_ext, $extensions)) {
-			$errors[] = 'Extension not allowed: ' . $file_name . ' ' . $file_type;
-		}
+// 		if (!in_array($file_ext, $extensions)) {
+// 			$errors[] = 'Extension not allowed: ' . $file_name . ' ' . $file_type;
+// 		}
 
-		if ($file_size > 2097152) {
-			$errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
-		}
+// 		if ($file_size > 2097152) {
+// 			$errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
+// 		}
 
 		if (empty($errors)) {
 			move_uploaded_file($file_tmp, $file);
